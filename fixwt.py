e20849508bd4790804453f0d9aa315a17923bad8
Meera Patel <mpatel@kazul.astro.wesleyan.edu>
2026-06-09 11:14:21 -0400

    Updated fixwt.py working

--- fixwt.py
+++ fixwt.py
@@ -5,12 +5,12 @@ from datetime import date
 top_dir = '/Volumes/disks/meerap/data/' #this is where the data lives
 targ_name = 'FPTau'
 tag = 'B4_hi'
-input_ms_name = 'FPTau_vis_B4_hi.ms' # name of measurement set to be copied
+input_ms_name = 'FPTau_B4_hi.contap1.ms' # name of measurement set to be copied
 
 data_dir = top_dir + targ_name + '/' # eg. this would give /Volumes/disks/meerap/data/FPTau/
-source_ms = data_dir + targ_name + '_vis_' + tag + '.ms' # eg. this would give /Volumes/disks/meerap/data/FPTau/FPTau_vis_B4_hi.ms
+source_ms = data_dir + targ_name + '_' + tag + '.contap1.ms' # eg. this would give /Volumes/disks/meerap/data/FPTau/FPTau_B4_hi.contap1.ms
 out_fol = data_dir + 'statwt_' + str(date.today()) + '/' # output folder where copied .ms will go, eg. /Volumes/disks/meerap/data/FPTau/statwt_2026-06-08/
-mscpy = out_fol + targ_name + '_vis_' + tag + '_wtfx.ms'
+mscpy = out_fol + targ_name + '_' + tag + '.contap1' + '_wtfx.ms'
 
 print('Original MS:')
 print(source_ms)
