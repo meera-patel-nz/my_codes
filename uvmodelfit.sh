0725e50dcbba388d32d9b6b6675f5ee86f1fa957
Meera Patel <mpatel@kazul.astro.wesleyan.edu>
2026-06-11 11:22:27 -0400

    Updated shape after tag for easy substitution when changing between G and D

--- uvmodelfit.sh
+++ uvmodelfit.sh
@@ -1,7 +1,8 @@
 import os
 
 top_dir = '/Volumes/disks/meerap/data/'
-targ_name = 'FPTau'
+targ_name = 'DSTau'
+shape = 'D' # G or D based on Gaussian or Disk 
 tag = 'B4_hi'
 cf_date = '2026-06-09' # date of the casa_fit
 
@@ -12,14 +13,14 @@ band_dir = data_dir + tag +'/' # band directory where .ms directory lives
 os.system('mkdir -p ' + cl_dir)
 
 vis = band_dir + targ_name + '_vis_' + tag + '.ms'
-outfile = cl_dir + targ_name + '_complist_G.cl'
+outfile = cl_dir + targ_name + '_complist_' + shape + '.cl'
 
 os.system('rm -rf ' + outfile) # delete the old version of the .cl directory before rerunning uvmodelfit
 
 uvmodelfit(
 vis= vis,
 field='',
-comptype='G',
+comptype=shape,
 sourcepar=[0.00244359,0,0,0.419465,0.53724387016,53.1382],
 outfile= outfile
 )
