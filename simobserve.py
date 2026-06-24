f6c8dac7e761f81a3d89bcd9dd9759b4354e6d84
Meera Patel <mpatel@kazul.astro.wesleyan.edu>
2026-06-24 09:12:15 -0400

    working simobserve code.

--- simobserve.py
+++ simobserve.py
@@ -43,19 +43,17 @@ cl.addcomponent(
     positionangle= posang,         # PA after correction
     shape='Gaussian'
 )
+cl.rename('DOTau_gaussian.cl')
+cl.close()
 
-
-project_folder = '/Volumes/disks/meerap/data/modelTau/DOTau_sim/'
+project_folder = 'DOTau_sim'
 skymodel_path = '/Volumes/disks/meerap/data/modelTau/DOTau_gaussian.image/'
 complist_path = '/Volumes/disks/meerap/data/modelTau/DOTau_gaussian.cl/'
 antennalist_path = '/soft/casa-6.6.0-20-py3.8.el7/lib/py/lib/python3.8/site-packages/casadata/__data__/alma/simmos/alma.cycle5.1.cfg'
 
 # use os system to copy /soft/casa-6.6.0-20-py3.8.el7/lib/py/lib/python3.8/site-packages/casadata/__data__/alma/simmos/alma.cycle5.1.cfg into the folder, DOTau_sim (project_folder) and .image and .cl are one directory above (in modelTau).
 
-
-# Delete old folder if exists - may need to move this up or down - will this delete the project i literally just made??
-
-# Delete old folder if it exists using rm -rf
+# Delete old folder if it already exists using rm -rf
 if os.path.exists(project_folder):
     os.system(f'rm -rf {project_folder}')
     print(f"Deleted previous simulation folder: {project_folder}")
@@ -80,18 +78,5 @@ simobserve(
         mapsize='10arcsec',
         incell='0.05arcsec',
         overwrite=True)
-
-from casatools import image
-ia = image()
-ia.open('DOTau_gaussian.image')
-shape = ia.shape()
-print("CASA image shape:", shape)
-ia.close()
-
-n_pix = mapsize / incell
-print("Pixels along one axis:", n_pix)
-
-print("Major axis:", maj)
-print("Minor axis:", minr)
 
 
