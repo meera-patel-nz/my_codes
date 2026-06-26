e25e5d34ba358a1af3cfdab34ecde367cbdb6b00
Meera Patel <mpatel@kazul.astro.wesleyan.edu>
2026-06-26 11:43:37 -0400

    Add tclean simulation script

---
+++ tclean_sim.sh
@@ -0,0 +1,31 @@
+from casatasks import tclean, imstat, visstat
+import os
+
+vis_no_noise = '/Volumes/disks/meerap/data/DOTau/simulation/DOTau_sim/DOTau_sim.alma.cycle5.1.ms'
+
+vis_noisy = '/Volumes/disks/meerap/data/DOTau/simulation/DOTau_sim/DOTau_sim.alma.cycle5.1.noisy.ms'
+
+imagename_no_noise = '/Volumes/disks/meerap/data/DOTau/simulation/DOTau_sim/DOTau_nonoise_dirty'
+
+imagename_noisy = '/Volumes/disks/meerap/data/DOTau/simulation/DOTau_sim/DOTau_noisy_dirty'
+
+# Delete previous tclean products using FULL PATHS
+os.system('rm -rf ' + imagename_no_noise + '.*')
+os.system('rm -rf ' + imagename_noisy + '.*')
+
+tclean(
+    vis= vis_noisy,
+    imagename= imagename_noisy,
+    spw='',
+    specmode='mfs',
+    gridder='standard',
+    imsize=[1024, 1024],
+    cell='0.02arcsec',
+    weighting="briggs",
+    robust=0.5,
+    niter=0,
+    interactive=False
+)
+
+     #pixel size?? - use grid from prev. fits or standard?
+
