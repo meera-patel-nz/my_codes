0f27b9ca9ad6912f91357c8dbd3b1f20cc6afbcd
Meera Patel <mpatel@kazul.astro.wesleyan.edu>
2026-07-09 21:42:24 -0400

    edited code, now has .npz file in subfolder, for MCMC purposes

--- simobserve.py
+++ simobserve.py
@@ -1,49 +1,28 @@
 from casatasks import simobserve
-from casatools import componentlist
 import os
+import subprocess
+import numpy as np
 
-sim_folder = '/Volumes/disks/meerap/data/DOTau/simulation'
-fits_file = os.path.join(sim_folder, 'DOTau_B4_hi_gaussian.fits')
-image_file = os.path.join(sim_folder, 'DOTau_gaussian.image')
-cl_file = os.path.join(sim_folder, 'DOTau_gaussian.cl')
-project_name = 'DOTau_sim'
-project_folder = os.path.join(sim_folder, project_name)
+trial = '020'
+sim_folder = '/Volumes/disks/meerap/data/DOTau/simulation/'
+fits_file = sim_folder + 'DOTau_B4_hi_gaussian.' + trial + '.fits'
+image_file = sim_folder + 'DOTau_gaussian.image'
+project_name = 'DOTau_sim_' + trial
+project_folder = sim_folder + project_name + '/'
 
 if os.path.exists(image_file):
     os.system(f'rm -rf {image_file}')
+    print(f"Deleted previous image: {image_file}")
 
 importfits(fitsimage=fits_file,
            imagename=image_file,
            overwrite=True)
 
-if os.path.exists(cl_file):
-    os.system(f'rm -rf {cl_file}')
-    print(f"Deleted previous component list: {cl_file}")
-    rmtables(cl_file)  # clean CASA tables if present
-
-if os.path.exists(project_folder):
-    os.system(f'rm -rf {project_folder}')
-os.makedirs(project_folder, exist_ok=True)
-
-#cl_file = '/Volumes/disks/meerap/data/DOTau/simulation/DOTau_gaussian.cl'
-#print('cl file name:', cl_file)
-#os.system(f'rm -rf {cl_file}')
-#cl.close()
-
-#if os.path.exists("cl_file"):
- #   rmtables("cl_file")
-  #  cl.done()
-
-# Delete previous CASA image if it exists
-#image_file = '/Volumes/disks/meerap/data/DOTau/simulation/DOTau_gaussian.image'
-#if os.path.exists(image_file):
-   # os.system(f'rm -rf {image_file}')
-  #  print(f"Deleted previous CASA image: {image_file}")
-
-# Import the Gaussian FITS
-#importfits(fitsimage='DOTau_B4_hi_gaussian.fits', 
-#           imagename='DOTau_gaussian.image',
- #          overwrite=True)
+"""
+if os.path.exists(project_name):
+    os.system(f'rm -rf {project_name}')
+    print(f"Deleted previous project folder: {project_name}")
+os.makedirs(project_name, exist_ok=True)
 
 # Create component list
 cl = componentlist()
@@ -51,52 +30,101 @@ cl = componentlist()
 PA = -66.6743 # rotation angle (p in deg)
 PA_cor=90-abs(PA)
 posang = f'{PA_cor}deg'
-majax= 0.393551
-minax = 0.393551*0.557551
+majax= 0.393551*0.5
+majaxst=f'{majax}arcsec'
+minax = (0.393551*0.557551)*0.5
 minaxst = f'{minax}arcsec' # FWHM minor axis × axis ratio string
-
+# CHANGING SIZE facTOR!!)
 cl.addcomponent(
     flux=0.0484384,                 # peak flux in Jy
     fluxunit='Jy',
     dir='J2000 10h00m00.0s -30d00m00.0s',  # source center
-    majoraxis='0.393551arcsec',     # FWHM major axis
+    majoraxis=majaxst,     # FWHM major axis
     minoraxis= minaxst,
     positionangle= posang,         # PA after correction
     shape='Gaussian'
 )
 cl.rename(cl_file)
 cl.close()
+"""
 
 antennalist_path = '/soft/casa-6.6.0-20-py3.8.el7/lib/py/lib/python3.8/site-packages/casadata/__data__/alma/simmos/alma.cycle5.1.cfg'
-
-# use os system to copy /soft/casa-6.6.0-20-py3.8.el7/lib/py/lib/python3.8/site-packages/casadata/__data__/alma/simmos/alma.cycle5.1.cfg into the folder, DOTau_sim (project_folder) and .image and .cl are one directory above (in modelTau).
-
-# Delete old folder if it already exists using rm -rf
-if os.path.exists(project_folder):
-    os.system(f'rm -rf {project_folder}')
-    print(f"Deleted previous simulation folder: {project_folder}")
-
-# Recreate the folder
-os.makedirs(project_folder, exist_ok=True)
-print(f"Created simulation folder: {project_folder}")
-
-#if os.path.exists(project_folder):
-#    os.system(f'rm -rf {project_folder}')
-
+antenna_config_name = 'alma.cycle5.1'
 #generate measurement set
 
 simobserve(
         project=project_name,
+        #complist=cl_file,
         skymodel=image_file,
-        complist=cl_file,
-        inbright='0.0484384Jy',
+        inwidth='7.5GHz',
         antennalist=antennalist_path,
         totaltime='180s',
         obsmode='int',
-        mapsize='10arcsec',
-        incell='0.05arcsec',
-        thermalnoise='tsys-atm',
-        user_pwv=0.5,
+        mapsize=['10arcsec'],
+        integration='10s',
+        thermalnoise='tsys-manual',
+        tau0=0.9,
         overwrite=True)
 
+# move DOTau_sim into data directory 
+subprocess.run(['mv', project_name, sim_folder], check=True)
+
+# --------- generate an npz_outfile to be used for MCMC chain -----------------
+
+ms_file = project_folder + project_name + '.' + antenna_config_name + '.noisy.ms'
+npz_outfile = project_folder + 'exported_vis_data_' + trial + '.npz'
+
+print('Reading ms file:', ms_file)
+
+# extract from measurement set
+ms.open(ms_file)
+d = ms.getdata(['u', 'v', 'data', 'weight', 'axis_info'])
+ms.close()
+
+u_m = d['u']
+v_m = d['v']
+vis = d['data']
+wgt = d['weight']
+freq_hz = d['axis_info']['freq_axis']['chan_freq']
+
+print('u_m shape:', u_m.shape)
+print('v_m shape:', v_m.shape)
+print('vis shape:', vis.shape)
+print('wgt shape:', wgt.shape)
+print('freq_hz shape:', freq_hz.shape)
+
+# average over polarization
+vis_avg = np.mean(vis, axis=0)
+wgt_avg = np.mean(wgt, axis=0)
+
+nchan = vis_avg.shape[0]
+nrow = vis_avg.shape[1]
+
+# build matching-shape grids for u, v, weight, freq
+freq_hz_grid = np.tile(freq_hz.reshape(nchan, 1), (1, nrow))
+u_m_grid = np.tile(u_m.reshape(1, nrow), (nchan, 1))
+v_m_grid = np.tile(v_m.reshape(1, nrow), (nchan, 1))
+wgt_grid = np.tile(wgt_avg.reshape(1, nrow), (nchan, 1))
+
+# convert u, v from meters to wavelengths
+c = 2.99792458e8
+wavelength_m = c / freq_hz_grid
+u_wave = u_m_grid / wavelength_m
+v_wave = v_m_grid / wavelength_m
+
+# flatten everything to 1D
+u_out = u_wave.flatten()
+v_out = v_wave.flatten()
+vis_out = vis_avg.flatten()
+wgt_out = wgt_grid.flatten()
+nu_out = freq_hz_grid.flatten()
+
+print('u_out shape:', u_out.shape)
+print('v_out shape:', v_out.shape)
+print('vis_out shape:', vis_out.shape)
+print('wgt_out shape:', wgt_out.shape)
+print('nu_out shape:', nu_out.shape)
+
+np.savez(npz_outfile, u=u_out, v=v_out, Vis=vis_out, Wgt=wgt_out, nu=nu_out)
+print('Saved to', npz_outfile)
 
